7b902ca0cabc13e3fa4bccc71aec4784b8ca0816
Md Imthiaj Habib <imthiaj.habib@gmail.com>
2013-02-05 09:39:59 +1100

    Title changed

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Git Test with Bootstrap, from Twitter</title>
+    <title>Git test successful</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
